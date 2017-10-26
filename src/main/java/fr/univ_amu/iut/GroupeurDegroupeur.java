c3400cb75035e6ab0882d659f57e3d0dfc49760d
guillaume-lagay <guillaume.lagay@etu.univ-amu.fr>
2017-10-26 12:22:50 +0200

    Groupeur en cours

--- src/main/java/fr/univ_amu/iut/GroupeurDegroupeur.java
+++ src/main/java/fr/univ_amu/iut/GroupeurDegroupeur.java
@@ -11,33 +11,22 @@ import static java.lang.StrictMath.log10;
 public class GroupeurDegroupeur {
     public ArrayList<Integer> grouperChaineAscii(ArrayList<Integer> chaineEnAscii) {
         ArrayList<Integer> listeGroupee = new ArrayList<Integer>();
-        for (int i = 0; i < listeGroupee.size()-1; i++) {
-            listeGroupee.set(i,chaineEnAscii.get(i));
+        for (int i = 0; i < listeGroupee.size() - 1; i++) {
+            listeGroupee.set(i, chaineEnAscii.get(i));
             int j = i;
             while ((log10(listeGroupee.get(i)) + 1) < 4) {
-                while (listeGroupee.get(j+1) == 0 && listeGroupee.get(j) != listeGroupee.get(listeGroupee.size()-1)) {
+                while (listeGroupee.get(j + 1) == 0 && listeGroupee.get(j) != listeGroupee.get(listeGroupee.size() - 1)) {
                     j = j + 1;
-                if (listeGroupee.get(j) == listeGroupee.get(chaineEnAscii.get(chaineEnAscii.size()-1))) {
-
-                }
+                    if (listeGroupee.get(j) == listeGroupee.get(chaineEnAscii.get(chaineEnAscii.size() - 1))) {
+                        //ici
+                    }
                 }
 
             }
 
 
-
-
-
-
-
-
-
-
-
-
-
         }
-
+        return listeGroupee;
     }
 
 }
