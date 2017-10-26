212cd642cb84a56d880a2aa3e850338a91157b47
p16005334 <baptiste-pauletto@etu.univ-amu.fr>
2017-10-26 11:22:51 +0200

    ManipulateurDeString fonctionnel

--- src/main/java/fr/univ_amu/iut/ManipulateurDeString.java
+++ src/main/java/fr/univ_amu/iut/ManipulateurDeString.java
@@ -1,7 +1,30 @@
 package fr.univ_amu.iut;
 
+import java.util.ArrayList;
+
 /**
  * Created by p16005334 on 20/10/17.
  */
 public class ManipulateurDeString {
+
+    public ManipulateurDeString() {
+        System.out.println("Manipulateur de string créé.");
+    }
+
+    public ArrayList<Integer> decouperString(String string)
+    {
+        ArrayList<Integer> chaineEnAscii = new ArrayList<Integer>();
+        for (int i = 0; i<string.length();i++ ) {
+            chaineEnAscii.add((int)string.charAt(i));
+        }
+        return chaineEnAscii;
+    }
+
+    public String reformerString(ArrayList<Integer> chaineDecodee){
+        String Message = new String();
+        for(Integer i : chaineDecodee){
+            Message += (char) (i.intValue());
+        }
+        return Message;
+    }
 }
