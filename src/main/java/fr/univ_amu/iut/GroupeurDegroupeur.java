b393a1b16828f4eb2391a165b88dff24cfb9a38b
Guillaume LAGAY <guillaume.lagay@etu.univ-amu.fr>
2017-11-01 01:16:05 +0100

    ça avance ça avance, petit push à 1h on est là

--- src/main/java/fr/univ_amu/iut/GroupeurDegroupeur.java
+++ src/main/java/fr/univ_amu/iut/GroupeurDegroupeur.java
@@ -1,5 +1,6 @@
 package fr.univ_amu.iut;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -11,18 +12,22 @@ import static java.lang.StrictMath.log10;
 public class GroupeurDegroupeur {
     public ArrayList<String> grouperChaineAscii(ArrayList<String> chaineEnAscii) {
         ArrayList<String> listeGroupee = new ArrayList<String>();
-        for (int i = 0; i < listeGroupee.size() - 1; i++) {
-            listeGroupee.set(i, chaineEnAscii.get(i));
+        for (int i = 0; i < chaineEnAscii.size() - 1; i++) {
+            listeGroupee.add(i, chaineEnAscii.get(i));
             int j = i;
             while (listeGroupee.get(i).length() < 4) {
-                while (listeGroupee.get(j + 1).equals(" ") && listeGroupee.get(j) != listeGroupee.get(listeGroupee.size() - 1)) {
+                while (chaineEnAscii.get(j+1).equals(" ")  && j != (listeGroupee.size() - 1))  {
+                    System.out.println("On incrémente j = " + j );
                     ++j;
+                    System.out.println(j);
                 }
-                if (listeGroupee.get(j) == listeGroupee.get(listeGroupee.size() - 1)) {
+                /*if (listeGroupee.get(j) == listeGroupee.get(listeGroupee.size() - 1)) {
                     break;
-                }
-                listeGroupee.add(i, chaineEnAscii.get(j + 1).substring(0, 1));
-                chaineEnAscii.set(j + 1, chaineEnAscii.get(j + 1).substring(chaineEnAscii.get(j + 1).length() - 1));
+                } */
+                listeGroupee.set(i, listeGroupee.get(i) + chaineEnAscii.get(j + 1).substring(0, 1));
+                chaineEnAscii.set(j + 1, chaineEnAscii.get(j + 1).substring(1));
+                System.out.println("liste groupée " + listeGroupee);
+                System.out.println("liste d'avant : " + chaineEnAscii);
             }
         }
         while (listeGroupee.get(listeGroupee.size() - 1).equals(" ")) {
@@ -34,12 +39,5 @@ public class GroupeurDegroupeur {
         }
         listeGroupee.set(listeGroupee.size() - 1, chainetmp);
         return listeGroupee;
-    }
-
-    public static void main(String[] args) {
-        String chainetmp = new String();
-        chainetmp = "01";
-        chainetmp = "0" + chainetmp;
-        System.out.println(chainetmp);
     }
 }
